d6e1161c3720a5ab3ffc7cb4a086794af9b4d635
edu ruela <eduardoruela@ciandt.com>
2024-08-02 16:30:15 -0300

    Primeira Classe em Phyton

---
+++ Laços.py
@@ -0,0 +1,10 @@
+# definindo uma lista de numeros
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# percorrendo a lista de números com loop for
+for numero in numeros:
+    # o numero é par?
+    if numero % 2 == 0:
+        print(f'{numero} é par')
+    else:
+        print(f'{numero} é impar')
