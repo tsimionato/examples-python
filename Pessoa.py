8d1c023e71aed6920c602478c14178b147be2713
edu ruela <eduardoruela@ciandt.com>
2024-08-09 16:13:17 -0300

    Codigo 08/08

---
+++ Pessoa.py
@@ -0,0 +1,27 @@
+class Pessoa:
+    def __init__(self, nome: str, idade: int, genero: str):
+        self.nome = nome
+        self.idade = idade
+        self.genero = genero
+
+    def exibir_informacoes(self) -> None:
+        print(f"Nome: {self.nome}, Idade: {self.idade}, Gênero: {self.genero}")
+
+
+pessoa1 = Pessoa("João", 30, "Masculino")
+pessoa2 = Pessoa("Maria", 25, "Feminino")
+pessoa3 = Pessoa("Eduardo", 23, "Masculino")
+pessoa4 = Pessoa("Thiago", 35, "Masculino")
+pessoa5 = Pessoa("Carl", 34, "Masculino")
+pessoa6 = Pessoa("Cherry", 41, "Feminino")
+pessoa7 = Pessoa("Camila", 17, "Feminino")
+pessoa8 = Pessoa("Paulo", 30, "Masculino")
+
+pessoa1.exibir_informacoes()
+pessoa2.exibir_informacoes()
+pessoa3.exibir_informacoes()
+pessoa4.exibir_informacoes()
+pessoa5.exibir_informacoes()
+pessoa6.exibir_informacoes()
+pessoa7.exibir_informacoes()
+pessoa8.exibir_informacoes()
