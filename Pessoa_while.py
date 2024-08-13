364914aaaa26272d4d1706e93c117e0ebbfeb47b
edu ruela <eduardoruela@ciandt.com>
2024-08-13 10:25:42 -0300

    While Pessoa

---
+++ Pessoa_while.py
@@ -0,0 +1,25 @@
+class Pessoa:
+    def __init__(self, nome: str, idade: int, genero: str):
+        self.nome = nome
+        self.idade = idade
+        self.genero = genero
+
+    def exibir_informacoes(self) -> None:
+        print(f"Nome: {self.nome}, Idade: {self.idade}, Gênero: {self.genero}")
+
+
+pessoa1 = Pessoa("João", 30, "Masculino")
+pessoa2 = Pessoa("Maria", 25, "Feminino")
+pessoa3 = Pessoa("Eduardo", 23, "Masculino")
+pessoa4 = Pessoa("Thiago", 35, "Masculino")
+pessoa5 = Pessoa("Carl", 34, "Masculino")
+pessoa6 = Pessoa("Cherry", 41, "Feminino")
+pessoa7 = Pessoa("Camila", 17, "Feminino")
+pessoa8 = Pessoa("Paulo", 30, "Masculino")
+
+pessoas = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5, pessoa6, pessoa7, pessoa8]
+
+i = 0
+while i < len(pessoas):
+    pessoas[i].exibir_informacoes()
+    i += 1
