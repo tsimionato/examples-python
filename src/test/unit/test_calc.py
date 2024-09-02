b1434dcea5471c9096f97ec79f1b0495b3518b37
Eduu-ru <gerenciaduruz@gmail.com>
2024-09-02 11:38:40 -0300

    banco py

---
+++ src/test/unit/test_calc.py
@@ -0,0 +1,27 @@
+
+def _test_calc_soma():
+    from src.Calc import Calc
+    calc = Calc()
+
+    assert calc == 4
+
+
+def _test_calc_subtracao():
+    from src.Calc import Calc
+    calc = Calc()
+
+    assert calc == 2
+
+
+def _test_calc_multiplicao():
+    from src.Calc import Calc
+    calc = Calc()
+
+    assert calc == 6
+
+
+def _test_calc_divisao():
+    from src.Calc import Calc
+    calc = Calc()
+
+    assert calc == 2
