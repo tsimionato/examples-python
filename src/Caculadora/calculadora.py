f81b7fd5223c1bea9c1a4addd99b65492b2b86b6
Eduu-ru <gerenciaduruz@gmail.com>
2024-09-03 17:59:59 -0300

    banco py

---
+++ src/Caculadora/calculadora.py
@@ -0,0 +1,15 @@
+
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
+
+    def subtracao(self, a, b):
+        return a - b
+
+    def multiplicacao(self, a, b):
+        return a * b
+
+    def divisao(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("Não é possível dividir por zero.")
+        return a / b
---
+++ tests/teste_calculadora.py
@@ -0,0 +1,37 @@
+# tests/test_calculadora.py
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from calculadora import Calculadora
+
+
+class TestCalculadora(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculadora()
+
+    def test_soma(self):
+        resul = self.calc.soma(5, 3)
+        self.assertEqual(resul, 8)
+
+    def test_subtracao(self):
+        resul = self.calc.subtracao(5, 3)
+        self.assertEqual(resul, 2)
+
+    def test_multiplicacao(self):
+        resul = self.calc.multiplicacao(5, 3)
+        self.assertEqual(resul, 15)
+
+    def test_divisao(self):
+        resul = self.calc.divisao(6, 3)
+        self.assertEqual(resul, 2)
+
+    def test_divisao_por_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divisao(5, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
